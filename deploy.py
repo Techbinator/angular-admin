372e7b738474d9c0bd57db212cde1edddc7fdd85
Tudor Filipovici <tudor.filipovici@holidaycheck.com>
2019-02-07 11:35:48 +0100

    Implement deployment and also fix production bug plus health path

---
+++ deploy.py
@@ -0,0 +1,15 @@
+import subprocess
+process = subprocess.Popen(
+    ['git', 'rev-parse', 'HEAD'], shell=False, stdout=subprocess.PIPE)
+git_head_hash = process.communicate()[0].strip()
+repo_name = "docker-registry.hc.ag/tm/sea-admin:" + git_head_hash
+
+subprocess.call(['docker', 'build', '-t', repo_name, '.'])
+subprocess.call(['docker', 'push', repo_name])
+
+subprocess.call(['dmc', 'deploy', '--app', '/tm/sea-admin', '--port',
+                 '80', '--image', repo_name, '--provider',
+                 'kubernetes', '--cluster', 'development-gce',
+                 '--memory', '1024', '--health', '/_health',
+                 '--strip-prefix', '"true"'])
+print "Done"
---
+++ src/app/health/health.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-health',
+  templateUrl: './health.component.html'
+})
+export class HealthComponent {
+  constructor() {}
+}
