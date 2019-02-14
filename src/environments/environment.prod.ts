cd934e6738afc89a739753fb7b3fc3b85be60247
Tudor Filipovici <tudor.filipovici@holidaycheck.com>
2019-02-14 14:56:59 +0100

    Clean angular admin

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  authEndpoint: 'http://proxy.k8s.prod.hcg.cloud/ice/auth-service/v2',
+  authEndpoint: '',
   fakeLogin: false
 };
--- src/environments/environment.staging.ts
+++ src/environments/environment.staging.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  authEndpoint: 'http://proxy.k8s.dev.hcg.cloud/ice/auth-service/v2',
+  authEndpoint: '',
   fakeLogin: false
 };
